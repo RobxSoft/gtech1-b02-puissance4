ad0aaf69f3d471cbaf48b8d2dd5245c8eb395b86
Emilio Guillemet <robxsoft@gmail.com>
2021-10-15 17:04:50 +0200

    Place symbol function added

--- main.c
+++ main.c
@@ -5,6 +5,7 @@
 #define gridX 6
 
 char gameGrid[gridX][gridY];
+char tokens[] = "ox";
 
 void repeatStr(char str[], int count) {
     for (int i = 0; i < count; i++) {
@@ -26,6 +27,22 @@ void printGrid() {
     repeatStr("_", gridY);
 }
 
+int placeSymbol(x,symbol) {
+    if (x > gridX){
+        return 2; // out of range
+    }else if(gameGrid[0][x] != '.'){
+        return 0; // full
+    }else{
+        for (int i = gridX; i >= 0; i--){
+            if (gameGrid[i][x] == '.'){
+                gameGrid[i][x] = symbol; // setting symbol
+                break;
+            }
+        } 
+    }
+    return 1;
+}
+
 void init(void) {
     for(int x = 0; x<gridX;x++){
         for(int y = 0; y < gridY; y++){
@@ -40,14 +57,16 @@ int main(void){
     int currentPlayerPlaying = 0;
 
     init();
-
+    printGrid();
     while (!gameFinished && emplacementLeft > 0){
         //printf("Next round\n");
         //printf("Player %d is playing \n", currentPlayerPlaying);
         emplacementLeft -= 1;
         currentPlayerPlaying = currentPlayerPlaying ^ 1;
+        printf("playing : %c\n", tokens[currentPlayerPlaying]);
+        placeSymbol(0,tokens[currentPlayerPlaying]);
+        printGrid();
     }
-    printGrid();
 
     return 1;
 }
